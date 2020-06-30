1fb5b70c86bcc14766893d21d89666a58f23d8e8
Shivam11-g <54078532+Shivam11-g@users.noreply.github.com>
2020-06-30 22:39:26 +0530

    Updates

---
+++ precache-manifest.4af57716dd44a0ceb47266baea000126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de35e7417ae3f002f8d3d75707cfd11",
+    "url": "/my-frames/index.html"
+  },
+  {
+    "revision": "c94c81456a9de060ce47",
+    "url": "/my-frames/static/css/main.a706deb1.chunk.css"
+  },
+  {
+    "revision": "493d2ac670ab269f1166",
+    "url": "/my-frames/static/js/2.4a52a9eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-frames/static/js/2.4a52a9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94c81456a9de060ce47",
+    "url": "/my-frames/static/js/main.f8e98909.chunk.js"
+  },
+  {
+    "revision": "eae2a620dca19288f17c",
+    "url": "/my-frames/static/js/runtime-main.9648fc39.js"
+  },
+  {
+    "revision": "c72ee525e580e1f2fda365050bbad09d",
+    "url": "/my-frames/static/media/s.c72ee525.jpg"
+  }
+]);
